3e204595ffd4cfcc817973b9a07a07e9783b9760
Nattawut Ruangvivattanaroj <nattawut.ru@gmail.com>
2017-05-29 14:39:00 +0700

    add gorup.go

---
+++ src/group.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+)
+
+func main() {
+
+	//[[cat c] [sat s] [mat m]]
+	re, err := regexp.Compile(`(.)at`)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	res := re.FindAllStringSubmatch("The cat bat sat on the mat.", -1)
+	fmt.Printf("%v", res)
+
+	s := "Mr. Nattawut Ruangvivattanaroj"
+	re1, err := regexp.Compile(`(Mr)(s)?\. (\w+) (\w+)`)
+	result := re1.FindStringSubmatch(s)
+
+	for k, v := range result {
+		fmt.Printf("%d. %s\n", k, v)
+	}
+	// Prints
+	// 1. Mr
+	// 2.
+	// 3. Nattawut
+	// 4. Ruangvivattanaroj
+}
--- src/match.go
+++ src/match.go
@@ -18,4 +18,16 @@ func main() {
 	} else {
 		fmt.Printf("No match ")
 	}
+
+	r2, err := regexp.Compile(`\d`)
+	if err != nil {
+		fmt.Printf("Error : Type not INT")
+		return
+	}
+
+	// Will print 'true':
+	fmt.Printf("%v", r2.MatchString("Seven times seven is 49."))
+	// Will print 'false':
+	fmt.Printf("%v", r2.MatchString("Seven times seven is forty-nine."))
+
 }
